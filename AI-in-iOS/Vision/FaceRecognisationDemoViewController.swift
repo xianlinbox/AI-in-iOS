effa011be6e50e6ff0929488cdfb180afe19a09b
xianlinbox <xianlinbox@gmail.com>
2018-02-05 12:30:10 -0600

    add image pick from Photo lib

--- AI-in-iOS/Vision/FaceRecognisationDemoViewController.swift
+++ AI-in-iOS/Vision/FaceRecognisationDemoViewController.swift
@@ -7,12 +7,37 @@
 //
 import UIKit
 
-class FaceRecognisationDemoViewController: UIViewController {
+class FaceRecognisationDemoViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imageView:UIImageView!
+    @IBOutlet var changeImageButton:UIButton!
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
+    }
+    @IBAction func pickImageFromLib() {
+        imagePicker.allowsEditing = false;
+        imagePicker.sourceType = .photoLibrary;
+        imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        self.present(imagePicker, animated: true, completion: nil)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+}
+
+//MARK: ImagePicker Delegate
+extension FaceRecognisationDemoViewController {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let newImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = newImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
