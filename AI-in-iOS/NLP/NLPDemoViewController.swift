f9d76a6d44f0a10ba418a3a6d061a6a85cd096ab
xianlinbox <xianlinbox@gmail.com>
2018-02-23 10:16:03 -0600

    add record button

--- AI-in-iOS/NLP/NLPDemoViewController.swift
+++ AI-in-iOS/NLP/NLPDemoViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import Speech
 class NLPDemoViewController: UIViewController, SFSpeechRecognizerDelegate {
     var textView:UITextView!
+    
     lazy var speechRecoginizer:SFSpeechRecognizer? = {
         if let recoginiser = SFSpeechRecognizer(locale: Locale(identifier: "en_US")){
             recoginiser.delegate = self
@@ -19,15 +20,17 @@ class NLPDemoViewController: UIViewController, SFSpeechRecognizerDelegate {
         }
     }()
     
-    override func viewWillAppear(_ animated: Bool) {
-        addTextArea();
+    
+    override func viewDidLoad() {
+        addTextArea()
+        addRecordButton()
     }
     
-    func performAnalysis(){
-        
+    @objc func recordButtonTapped(){
+        print("record button tapped .........")
     }
     
-    private func addTextArea() {
+    fileprivate func addTextArea() {
         self.textView?.removeFromSuperview()
         textView = UITextView(frame: CGRect(x: 10, y: 10, width: UIConstants.SCREEN_WIDTH - 20, height: 100))
         textView.textColor = UIColor.red
@@ -35,6 +38,15 @@ class NLPDemoViewController: UIViewController, SFSpeechRecognizerDelegate {
         textView.layer.borderWidth = 1
         self.view.addSubview(textView)
     }
+    
+    fileprivate func addRecordButton() {
+        let recordButton = UIButton(frame: CGRect(x: UIConstants.SCREEN_WIDTH/2 - 100, y: UIConstants.SCREEN_HEIGHT - 100, width: 200, height: 30))
+        recordButton.setTitle("Start voice recording", for: .normal)
+        recordButton.backgroundColor=UIColor.blue
+        recordButton.addTarget(self, action: #selector(recordButtonTapped), for: .touchUpInside)
+        self.view.addSubview(recordButton)
+    }
+    
 }
 
 //MARK: SFSpeechRecoginizerDelegate
