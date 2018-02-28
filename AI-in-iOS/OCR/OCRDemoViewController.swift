78c52c33a18eab81aaf9d753c05a6094740cdddd
xianlinbox <xianlinbox@gmail.com>
2018-02-27 21:32:23 -0500

    add the image to project & implement the default imageAnalysis code

--- AI-in-iOS/OCR/OCRDemoViewController.swift
+++ AI-in-iOS/OCR/OCRDemoViewController.swift
@@ -7,5 +7,28 @@
 //
 import UIKit
 
-class OCRDemoViewController: UIViewController {
+class OCRDemoViewController: ImageAnalysisBaseViewController {
+    var textView:UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        self.reloadImage(newImage: UIImage(named: "OCRText"))
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        addTextArea();
+    }
+    
+    override func performAnalysis() {
+        
+    }
+    
+    private func addTextArea() {
+        self.textView?.removeFromSuperview()
+        let imageViewFrame = self.imageView.frame
+        textView = UITextView(frame: CGRect(x: imageViewFrame.origin.x + 10, y: imageViewFrame.origin.y + imageViewFrame.size.height + 10, width: imageViewFrame.size.width-20, height: 200))
+        textView.textColor = UIColor.red
+        textView.layer.borderColor = UIColor.black.cgColor
+        textView.layer.borderWidth = 1
+        self.view.addSubview(textView)
+    }
 }
