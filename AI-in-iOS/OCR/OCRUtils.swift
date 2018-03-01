15cd8152506a3d22a4c8ec27e7848749f4abfa86
xianlinbox <xianlinbox@gmail.com>
2018-03-01 16:00:03 -0500

    add OCRUtils * OCRUtilsTest

---
+++ AI-in-iOS/OCR/OCRUtils.swift
@@ -0,0 +1,16 @@
+//
+//  OCRUtils.swift
+//  AI-in-iOS
+//
+//  Created by Xianning Liu  on 01/03/2018.
+//  Copyright © 2018 Xianning Liu . All rights reserved.
+//
+
+import Foundation
+import TesseractOCR
+
+struct OCRUtils {
+    static func recogText(image:UIImage,_ completion:(String) -> Void){
+        
+    }
+}
